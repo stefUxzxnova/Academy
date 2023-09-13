7465bdb062f57c4c48eb1c302532832a0557a58f
DjUziMix <chiche2002@abv.bg>
2023-09-14 01:11:41 +0300

    FirstLessons

---
+++ ConditionalStatements/ConditionalStatements/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //speed
+        Scanner scanner = new Scanner(System.in);
+//        int distance = Integer.parseInt(scanner.nextLine());
+//        int hours = Integer.parseInt(scanner.nextLine());
+//        int minutes = Integer.parseInt(scanner.nextLine());
+//        int seconds = Integer.parseInt(scanner.nextLine());
+//
+//        double totalTimeInSeconds = hours * 3600 + (minutes * 60) + seconds;
+//
+//
+//        System.out.printf("Hello and welcome!");
+//        System.out.printf("%.6f", distance/totalTimeInSeconds);
+
+        int kgPaint = scanner.nextInt();
+    }
+}
