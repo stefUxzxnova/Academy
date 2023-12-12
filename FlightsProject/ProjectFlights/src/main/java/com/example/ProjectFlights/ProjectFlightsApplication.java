ed0dca67725cae79d5cc4186dc9c8dc420cc1398
stefUxzxnova <chiche2002@abv.bg>
2023-12-12 18:38:53 +0200

    FlightsProject initial commit

---
+++ FlightsProject/ProjectFlights/src/main/java/com/example/ProjectFlights/ProjectFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFlightsApplication.class, args);
+	}
+
+}
---
+++ FlightsProject/ProjectFlights/src/test/java/com/example/ProjectFlights/ProjectFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectFlights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
