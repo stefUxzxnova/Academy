a9dd34f0bf350a99cd35aba87525921355839ed1
stefUxzxnova <chiche2002@abv.bg>
2023-12-14 02:00:04 +0200

    Add DTOs

---
+++ FlightsProject/ProjectFlights/src/main/java/com/example/ProjectFlights/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package com.example.ProjectFlights.dto;
+
+import jakarta.persistence.Column;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
