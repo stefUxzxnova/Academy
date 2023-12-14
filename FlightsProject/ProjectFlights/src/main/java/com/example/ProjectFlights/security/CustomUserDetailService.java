0951a0fea16a16921bde3453b5b6ed82bb11a2d5
stefUxzxnova <chiche2002@abv.bg>
2023-12-14 02:02:54 +0200

    Make custom classes for User details and user details service

---
+++ FlightsProject/ProjectFlights/src/main/java/com/example/ProjectFlights/security/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.example.ProjectFlights.security;
+
+import com.example.ProjectFlights.model.User;
+import com.example.ProjectFlights.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        return new CustomUserDetails(user);
+    }
+}
