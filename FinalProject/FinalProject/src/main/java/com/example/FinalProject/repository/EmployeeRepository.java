2ff6fbb3f760154ca43363e72627d72dfbeb4985
stefUxzxnova <chiche2002@abv.bg>
2023-12-23 09:21:18 +0200

    Add repositories for employee and project

---
+++ FinalProject/FinalProject/src/main/java/com/example/FinalProject/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.FinalProject.repository;
+
+import com.example.FinalProject.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
